d4608038937bec5060549d353d44621dc3cb8113
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-19 20:02:35 +0800

    Create sol.cpp

---
+++ sol.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	ll w1, h1, w2, h2;
+	cin >> w1 >> h1 >> w2 >> h2;
+	cout << 2*(w1+h1+h2)+4 << "\n";
+}
